d06c61638d28b2d64dbde2b5d68a688a68bfd07c
dumol <dumol@gnome.org>
2020-12-14 13:40:44 +0200

    Try uploading without any cd to not need allow-list permission.

--- functions_pythia.sh
+++ functions_pythia.sh
@@ -188,10 +188,9 @@ build_publish_dist_sftp_batch() {
     # This matches the GitHub's hierarchy for production packages.
     local upload_version_dir="$PYTHON_BUILD_VERSION.$PYTHON_PACKAGE_VERSION"
 
-    echo "lcd dist/python/$OS/$ARCH/"        > publish_dist_sftp_batch
-    echo "cd testing"                       >> publish_dist_sftp_batch
-    echo "-mkdir $upload_version_dir"       >> publish_dist_sftp_batch
-    echo "cd $upload_version_dir"           >> publish_dist_sftp_batch
+    # Files are uploaded with a temp name and then renamed to final name.
+    echo "lcd dist/python/$OS/$ARCH/" > publish_dist_sftp_batch
+    echo "-mkdir testing/$upload_version_dir" >> publish_dist_sftp_batch
     echo "put python-$PYTHON_BUILD_VERSION.$PYTHON_PACKAGE_VERSION-$OS-$ARCH.tar.gz python-$PYTHON_BUILD_VERSION.$PYTHON_PACKAGE_VERSION-$OS-$ARCH.tar.gz.part" >> publish_dist_sftp_batch
-    echo "rename python-$PYTHON_BUILD_VERSION.$PYTHON_PACKAGE_VERSION-$OS-$ARCH.tar.gz.part python-$PYTHON_BUILD_VERSION.$PYTHON_PACKAGE_VERSION-$OS-$ARCH.tar.gz" >> publish_dist_sftp_batch
+    echo "rename testing/$upload_version_dir/python-$PYTHON_BUILD_VERSION.$PYTHON_PACKAGE_VERSION-$OS-$ARCH.tar.gz.part testing/$upload_version_dir/python-$PYTHON_BUILD_VERSION.$PYTHON_PACKAGE_VERSION-$OS-$ARCH.tar.gz" >> publish_dist_sftp_batch
 }
