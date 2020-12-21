e697439cd8a621df7ce38e2596e36c6ed9c3e0ab
dumol <dumol@gnome.org>
2020-12-21 11:04:19 +0200

    Build the SFTP batch file in build/.

--- functions_pythia.sh
+++ functions_pythia.sh
@@ -266,7 +266,7 @@ build_publish_dist_sftp_batch() {
 
     # $upload_dir exists if this is not the first upload for this version,
     # so the mkdir command is prefixed with '-' to allow it to fail.
-    echo "-mkdir $upload_dir"                         > publish_dist_sftp_batch
-    echo "put $local_dir/$pkg_file $temp_file"       >> publish_dist_sftp_batch
-    echo "rename $temp_file $dest_file"              >> publish_dist_sftp_batch
+    echo "-mkdir $upload_dir"                   > build/publish_dist_sftp_batch
+    echo "put $local_dir/$pkg_file $temp_file" >> build/publish_dist_sftp_batch
+    echo "rename $temp_file $dest_file"        >> build/publish_dist_sftp_batch
 }
