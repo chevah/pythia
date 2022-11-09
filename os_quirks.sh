17a55a80875408db98cb917df66d20a30b0e7034
dumol <dumol@gnome.org>
2022-11-09 11:08:58 +0200

    CentOS 5 identification fix.

--- os_quirks.sh
+++ os_quirks.sh
@@ -23,19 +23,10 @@ case $OS in
             # The busybox ersatz binary on Alpine Linux is different.
             export SHA_CMD="sha512sum -csw"
         elif [ -f /etc/redhat-release ]; then
-            echo "Red Hat MATCHED!"
-            echo "Content of release file:"
-            cat /etc/redhat-release
-            echo "Output of rpm command:"
-            rpm -E %{rhel}
-            if [ $(rpm -E %{rhel}) -eq 5 ]; then
+            if grep -q "CentOS release 5" /etc/redhat-release; then
                 # There are issues with Let's Encrypt certs on CentOS 5.
                 export GET_CMD="curl --silent --insecure --location --output"
             fi
-        else
-            echo "CentOS NOT MATCHED!"
-            echo "Files in /etc ending with release:"
-            ls -l /etc/*release
         fi
         # Build as portable as possible, only glibc 2.x should be needed.
         export BUILD_LIBFFI="yes"
