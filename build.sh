87050afb469b6abe57a491c41140677135dc4903
dumol <dumol@gnome.org>
2024-06-11 10:45:31 +0000

    Use gcc10 on Amazon Linux 2.

--- build.sh
+++ build.sh
@@ -48,9 +48,9 @@ PYTHON_BIN="$INSTALL_DIR/bin/$PYTHON_VERSION"
 
 # Explicitly choose the C compiler in order to make it possible to switch
 # between native compilers and GCC on platforms such as the BSDs and Solaris.
-export CC="gcc"
+export CC="${CC:-gcc}"
 # Used for testing Python C++ extensions (test_cppext).
-export CXX="g++"
+export CXX="${CXX:-g++}"
 # Other needed tools (GNU flavours preferred).
 # For proper quoting, _CMD vars are Bash arrays of commands and optional flags.
 MAKE_CMD=(make)
