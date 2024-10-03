bf5f05b0b32ad870098f65339518c99221144581
dumol <dumol@gnome.org>
2024-10-03 08:58:22 +0000

    The unzip ersatz in Alpine Linux busybox is good enough.

--- pkg_checks.sh
+++ pkg_checks.sh
@@ -15,15 +15,15 @@
 # On platforms with multiple C compilers, choose by setting CC in os_quirks.sh.
 
 # List of OS packages required for building Python/pyOpenSSL/cryptography etc.
-BASE_PKGS="gcc make m4 patch unzip perl"
+BASE_PKGS="gcc make m4 patch perl"
 if [ "$BUILD_LIBEDIT" = "yes" ]; then
     BASE_PKGS="$BASE_PKGS automake libtool"
 fi
 APK_PKGS="$BASE_PKGS git curl bash musl-dev linux-headers lddtree \
-    openssh-client file unzip g++ musl-locales dejagnu"
-DEB_PKGS="$BASE_PKGS tar diffutils git curl \
+    openssh-client file g++ musl-locales dejagnu"
+DEB_PKGS="$BASE_PKGS unzip tar diffutils git curl \
     openssh-client libtest-simple-perl xz-utils g++ dejagnu"
-RPM_PKGS="$BASE_PKGS tar diffutils git-core curl \
+RPM_PKGS="$BASE_PKGS bzip2 unzip tar diffutils git-core curl \
     openssh-clients perl-Test-Simple perl-IPC-Cmd xz gcc-c++ dejagnu"
 
 # Check for OS packages required for the build.
@@ -54,7 +54,7 @@ case "$OS" in
         ;;
     linux*)
         if [ -x /sbin/apk ]; then
-            # Assumes Alpine Linux 3.12.
+            # Assumes Alpine Linux 3.15.
             CHECK_CMD=(apk info -q -e)
             PACKAGES="$APK_PKGS"
         elif [ -x /usr/bin/dpkg ]; then
